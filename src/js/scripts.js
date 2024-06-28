538492909d1cec196f0bff77adf1b51e4e4e23e2
Nuri12c <nurislam.ganieiev@mail.ru>
2024-06-28 16:15:41 +0300

    Исправил все баги

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,37 +1,94 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cat = document.getElementById('cat')
+  const cat = document.getElementById("cat");
   const chair = document.getElementById("chair");
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener("keydown", function (event) {
     jump();
-  })
+  });
   document.addEventListener("touchstart", function (event) {
     jump();
   });
   function jump() {
-    if (cat.classList != 'jump') {
-      cat.classList.add('jump')
+    if (cat.classList != "jump") {
+      cat.classList.add("jump");
       cat.style.backgroundImage = "url(./src/assets/img/catJump.png)";
     }
     setTimeout(function () {
       cat.style.backgroundImage = "url(./src/assets/img/catJump2.png)";
     }, 300);
-    setTimeout (function() {
-      cat.classList.remove('jump')
-      cat.style.backgroundImage = "url(./src/assets/img/myCat.gif)";
-    }, 700)
-    
-  }
-  
-});
-let isAlive = setInterval( function() {
-  let catTop = parseInt(window.getComputedStyle(cat).getPropertyValue("top"));
-  let chairLeft = parseInt(window.getComputedStyle(chair).getPropertyValue("left"));
-
-  if (chairLeft < 50 && chairLeft > 0 && catTop >= 330) {
-    alert("GAME OVER");
-    chair.classList.remove('chairMov')
     setTimeout(function () {
-      chair.classList.add("chairMov");
-    }, 0);
+      cat.classList.remove("jump");
+      cat.style.backgroundImage = "url(./src/assets/img/myCat.gif)";
+    }, 700);
   }
-}, 10);
+
+  function checkResolutionAndDoSomething() {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+     let isAlive = setInterval(function () {
+       let catTop = parseInt(
+         window.getComputedStyle(cat).getPropertyValue("top")
+       );
+       let chairLeft = parseInt(
+         window.getComputedStyle(chair).getPropertyValue("left")
+       );
+
+       if (chairLeft < 50 && chairLeft > 0 && catTop >= 330) {
+         alert("GAME OVER");
+         chair.classList.remove("chairMov");
+         setTimeout(function () {
+           chair.classList.add("chairMov");
+         }, 0);
+       }
+     }, 10);
+    }
+  }
+
+
+  window.addEventListener("resize", checkResolutionAndDoSomething);
+
+  checkResolutionAndDoSomething();
+
+  function checkResolutionAndDoSomething() {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+     let isAlive = setInterval(function () {
+       let catTop = parseInt(
+         window.getComputedStyle(cat).getPropertyValue("top")
+       );
+       let chairLeft = parseInt(
+         window.getComputedStyle(chair).getPropertyValue("left")
+       );
+
+       if (chairLeft < 50 && chairLeft > 0 && catTop >= 330) {
+         alert("GAME OVER");
+         chair.classList.remove("chairMov");
+         setTimeout(function () {
+           chair.classList.add("chairMov");
+         }, 0);
+       }
+     }, 10);
+    } else {
+      let isAlive = setInterval(function () {
+        let catTop = parseInt(
+          window.getComputedStyle(cat).getPropertyValue("top")
+        );
+        let chairLeft = parseInt(
+          window.getComputedStyle(chair).getPropertyValue("left")
+        );
+
+        if (chairLeft < 50 && chairLeft > 0 && catTop >= 130) {
+          alert("GAME OVER");
+          chair.classList.remove("chairMov");
+          setTimeout(function () {
+            chair.classList.add("chairMov");
+          }, 0);
+        }
+      }, 10);
+    }
+  }
+
+
+  window.addEventListener("resize", checkResolutionAndDoSomething);
+
+  checkResolutionAndDoSomething();
+
+});
+
